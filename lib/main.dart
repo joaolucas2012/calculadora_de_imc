da81a218c4f6994ff2cc32696bcf2c3a8cf197ae
@joaolucas-prime <ribeirojoaolucas68@gmail.com>
2023-09-05 23:05:39 -0300

    :rocket: create project

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: const Text(
+          "Bem vindo ao app!",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
